262f1de28879d2f7edeb76017e545206785f5291
Spark <79936503+spa5k@users.noreply.github.com>
2022-04-07 13:12:32 +0530

    feat(route): ✨ certain datetime done

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from "fastify";
+import dateController from "./routes/dateController";
 import indexController from "./routes/indexController";
 import userController from "./routes/userController";
 
 export default async function router(fastify: FastifyInstance) {
-  fastify.register(userController, { prefix: "/api/v1/user" });
+  fastify.register(userController, { prefix: "/user" });
   fastify.register(indexController, { prefix: "/" });
+  fastify.register(dateController, { prefix: "/date" });
 }
---
+++ src/routes/dateController.ts
@@ -0,0 +1,37 @@
+import dayjs from "dayjs";
+import { FastifyInstance } from "fastify";
+import sql from "../config/sql";
+
+interface IQuerystring {
+  date: Date;
+}
+
+export default async function dateController(fastify: FastifyInstance) {
+  //     └── date (GET)
+  //         └── / (GET)
+  fastify.get<{
+    Querystring: IQuerystring;
+  }>("/", async (request, reply) => {
+    const date = request.query.date;
+    // Extract hour, mins, am pm from date
+    const hour = dayjs(date).hour();
+    const min = dayjs(date).minute();
+    const amPm = hour >= 12 ? "pm" : "am";
+    const time = `${hour > 12 ? hour % 12 : hour}:${min}${amPm}`;
+    const dayInNumber = dayjs(date).day();
+
+    let restaurant;
+    try {
+      restaurant = await sql`
+        select * from restaurant join opening_hours on restaurant.id=opening_hours.restaurant_id where day=${
+          dayInNumber + 1
+        } and hours @> ${time}::time`;
+    } catch (err) {
+      console.error(err);
+    }
+
+    reply.send({
+      restaurant,
+    });
+  });
+}
