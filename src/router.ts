a6c1c3ebe70d0d01dbf9f6cc282c1c48e0888386
Spark <79936503+spa5k@users.noreply.github.com>
2022-04-07 16:42:43 +0530

    feat(route): ✨ restaurant specific and combined search done

--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,20 @@
 import { FastifyInstance } from "fastify";
+import combinedSearchController from "./routes/combinedSearchController";
 import dateController from "./routes/dateController";
 import indexController from "./routes/indexController";
 import searchMenuController from "./routes/searchMenu";
+import searchRestaurantController from "./routes/searchRestaurant";
 import userController from "./routes/userController";
 
 export default async function router(fastify: FastifyInstance) {
   fastify.register(userController, { prefix: "/user" });
   fastify.register(indexController, { prefix: "/" });
   fastify.register(dateController, { prefix: "/date" });
+  fastify.register(combinedSearchController, {
+    prefix: "/search",
+  });
   fastify.register(searchMenuController, { prefix: "/search/menu" });
+  fastify.register(searchRestaurantController, {
+    prefix: "/search/restaurant",
+  });
 }
---
+++ src/routes/searchRestaurant.ts
@@ -0,0 +1,27 @@
+import { FastifyInstance } from "fastify";
+import sql from "../config/sql";
+
+interface IQuerystring {
+  name: string;
+}
+
+export default async function searchRestaurantController(
+  fastify: FastifyInstance
+) {
+  //     └── date (GET)
+  //         └── / (GET)
+  fastify.get<{
+    Querystring: IQuerystring;
+  }>("/", async (request, reply) => {
+    const { name } = request.query;
+    // Extract hour, mins, am pm from date
+
+    const data = await sql`
+    select r.name as "restaurantName", r.id as "restaurantId" from menu m join restaurant r on r.id=m.restaurant_id where r.document @@ websearch_to_tsquery(${name});
+    `;
+
+    reply.send({
+      name: data,
+    });
+  });
+}
